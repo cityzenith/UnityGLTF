0c2cac9c87645eaf5ef98194cdd83481dc930bfd
Unknown <d.mosquera@cityzenith.com>
2017-10-13 15:09:50 -0500

    Added Unity dll nuget package

--- GLTFSerialization/GLTFSerialization/Properties/AssemblyInfo.cs
+++ GLTFSerialization/GLTFSerialization/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GLTFSerialization")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GLTF Serialization library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kronos Group and Zityzenith")]
 [assembly: AssemblyProduct("GLTFSerializer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
